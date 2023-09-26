eae329226d951ac6d9c8639a9098c76434e0eebf
idoudi2020 <idoudimokhtar@.com>
2023-09-26 10:48:09 -0700

    7-edges.py

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = (word [0:3])
-word_last_2  = (word [7:])
-middle_word  = (word [1:8])
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
