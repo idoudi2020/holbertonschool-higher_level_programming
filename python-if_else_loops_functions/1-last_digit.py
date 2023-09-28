2d212f2772214120823320a209ff2ed010573ab1
idoudi2020 <idoudimokhtar@.com>
2023-09-28 05:17:01 -0700

    1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,13 @@
-
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 
-num_str = repr (number)
-last_digit_str = num_str [-1]
-last_digit = int (last_digit_str)
+last_digit = (abs(number) % 10) * (-1 if number < 0 else 1 )
 
 if (last_digit == 0 ):
     print("Last digit of", number ,"is" ,last_digit, "and is 0" ) 
-elif  ( last_digit < 6 and not 0 ):
+elif ( (last_digit < 6) and not 0 ): 
     print("Last digit of", number ,"is" ,last_digit, "and is less than 6 and not 0" )
+
 else:
     print("Last digit of", number ,"is" ,last_digit, "and is greater than 5" )
