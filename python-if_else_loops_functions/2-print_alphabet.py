8b829333b8eb27187c577ec8b0b8bde893f90fdf
idoudi2020 <idoudimokhtar@.com>
2023-09-28 12:01:19 -0700

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 for x in  range(97,123):
     print("{:c}".format(x), end="")
-''' 
+
+"""
 same code avec 
-#print(f'{chr(x)}', end="")
-#print("{}".format(chr(x)), end="")
-'''
+print(f'{chr(x)}', end="")
+print("{}".format(chr(x)), end="")
+
+"""
