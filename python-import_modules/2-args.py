3d0e48415ee12a98f2d9909a4a567080b9d1c06a
idoudi2020 <idoudimokhtar@.com>
2023-09-29 14:54:56 -0700

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 from sys import argv
 if __name__ == '__main__':
     num_arg = len(argv) - 1
-                arg_word = 'argument' if num_arg == 1 else 'arguments'
-                punctuation = '.' if num_arg == 0 else ':'
-                print(f"{num_arg} {arg_word}{punctuation}")
-                for i in range(1, num_arg + 1):
-                    print(f"{i}{':'} {argv[i]}")
+    arg_word = 'argument' if num_arg == 1 else 'arguments'
+    punctuation = '.' if num_arg == 0 else ':'
+    print(f"{num_arg} {arg_word}{punctuation}")
+    for i in range(1, num_arg + 1):
+        print(f"{i}{':'} {argv[i]}")
