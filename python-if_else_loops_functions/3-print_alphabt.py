505288e4add4303a9302f7b49fcfcf11eab25ab8
idoudi2020 <idoudimokhtar@.com>
2023-10-02 12:53:24 -0700

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -4,3 +4,10 @@ for x in range(97, 123):
     if (x == 101 or x == 113):
         continue
     print("{:c}".format(x), end='')
+
+    """
+for x in range(1, 10):
+    if (x != 101 or x != 113):
+        print("{:d}".format(x), end='')
+
+  """      
