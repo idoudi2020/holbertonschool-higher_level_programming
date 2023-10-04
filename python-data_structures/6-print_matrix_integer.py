e4365b284d3ae927cbf6fb753201aa115e4c0e12
idoudi2020 <idoudimokhtar@.com>
2023-10-04 04:57:51 -0700

    6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for raw in matrix:
+        if len(raw) == 0:
+            print("")
+                for i in range(len(raw)):
+                    if i == len(raw) - 1:
+                        print("{:d}".format(raw[i]))
+        else:
+            print("{:d}".format(raw[i]), end=' ')
