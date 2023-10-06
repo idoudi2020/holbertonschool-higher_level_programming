ddc8499a22c58b84c6a8e4029c1c8d479d8d8ea9
idoudi2020 <idoudimokhtar@gmail>
2023-10-06 21:26:19 +0200

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,67 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) != str:
+        return 0
     Roman_numerals = {'I': 1,'V': 5,  'X': 10, 'L': 50, 'C': 100, 'D': 500,  'M': 1000  }
     result = 0
     prev_value = 0
 
     for char in reversed(roman_string):
-        value = Roman_numerals.get(char, 0)
+        value = Roman_numerals.get(char)
+        if value == 0:
+            return 0
         if value < prev_value:
             result -= value
         else:
             result += value
-        prev_value = value
+            prev_value = value
 
     return result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
