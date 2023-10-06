fdb20d59a67824af96cdf8fae4af2c2944baa7ba
idoudi2020 <idoudimokhtar@gmail>
2023-10-06 20:49:25 +0200

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        hold_max = my_list[0]
+        for i in my_list:
+            if hold_max < i:
+                hold_max = i
+        return hold_max
+    else:
+        return None
