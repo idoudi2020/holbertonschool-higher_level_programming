b7123dc1e4aab3d97cf14ca45628c3f0294a2f8c
idoudi2020 <idoudimokhtar@gmail>
2023-10-06 20:46:35 +0200

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     return(len(sentence), sentence[0] if sentence else None)
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
