fcae98c47b444b210555d2c3d28804fbac0cb868
idoudi2020 <idoudimokhtar@gmail>
2023-10-11 13:41:17 +0200

    Python Classes and Objects

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Author: [Idoudi Mokhtar]
+Date: [10/10/2023]
+Description: This module defines the Square class.
+"""
+
+
+class Square:
+    """
+    A class that represents a square.
+
+    """
+    pass
